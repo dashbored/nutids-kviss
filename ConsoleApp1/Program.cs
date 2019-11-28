425e208b61c0b3148ef0eb29aa49177010d5242a
Dashbored <richar_davy@hotmail.com>
2019-11-28 09:08:36 +0100

    Added htmlmarkup and fixed id's on questions

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
     {
         static async Task Main(string[] args)
         {
-            var scraper = new WebScraper();
+            var scraper = new WebScraperService();
             var result = await scraper.ScrapeWebsite("https://www.dn.se/nyheter/nutidstestet/nutidstestet-vecka-44-6/");
             Console.ReadLine();            
         }
--- NutidsKviss/Startup.cs
+++ NutidsKviss/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DNWebScraper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -29,6 +30,7 @@ namespace NutidsKviss
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<WebScraperService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
